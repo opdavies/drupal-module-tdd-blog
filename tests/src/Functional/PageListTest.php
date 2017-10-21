f713d225bf6221372caddfbf85f461def5d3e918
Oliver Davies <oliver@oliverdavies.uk>
2017-10-21 20:23:05 +0100

    Add correct nodes test

--- tests/src/Functional/PageListTest.php
+++ tests/src/Functional/PageListTest.php
@@ -21,4 +21,19 @@ class PageListTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
   }
 
+  /**
+   * Ensure that only the correct nodes are returned.
+   *
+   * Ensure that only published pages are returned by the view. Unpublished
+   * pages or content of different types should not be shown.
+   */
+  public function testOnlyPublishedPagesAreShown() {
+    // Given that a have a mixture of published and unpublished pages, as well
+    // as other types of content.
+
+    // When I view the page.
+
+    // Then I should only see the published pages.
+  }
+
 }
