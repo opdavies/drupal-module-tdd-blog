05cd5901d5176246cbb5c2546d33b8221de63c7c
Oliver Davies <oliver@oliverdavies.uk>
2017-10-21 17:35:54 +0100

    Add test structure

---
+++ tests/src/Functional/PageListTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\tdd_dublin\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+class PageListTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['tdd_dublin'];
+
+}
